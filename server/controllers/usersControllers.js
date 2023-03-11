3d3afb2489cc926a157e27fba793a734fb849c55
Manohisoa <manohisoarajaonarivony@gmail.com>
2023-03-11 15:03:35 +0300

    created user controllers

---
+++ server/controllers/usersControllers.js
@@ -0,0 +1,42 @@
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+import User from "../models/user";
+
+dotenv.config();
+
+const SECRET = process.env.SECRET;
+
+async function signup(req, res) {
+  const user = new User(req.body);
+  try {
+    await user.save();
+    const token = createJWT(user);
+    res.json({ token });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json(error);
+  }
+}
+
+async function login(req, res) {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) return res.status(401).json({ err: "Email not found" });
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (isMatch) {
+        const token = createJWT(user);
+        res.json({ token });
+      } else {
+        return res.status(401).json({ err: "Invalid password" });
+      }
+    });
+  } catch (error) {
+    return res.status(401).json(error);
+  }
+}
+
+function createJWT(user) {
+  return jwt.sign({ user }, SECRET, { expiresIn: "24h" });
+}
+
+export { signup, login };
