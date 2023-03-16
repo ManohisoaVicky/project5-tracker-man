533b51caf03559121aafb3e20127eae095d26c1d
Manohisoa <manohisoarajaonarivony@gmail.com>
2023-03-16 17:30:05 +0300

    added user context

---
+++ client/src/context/UserContext.js
@@ -0,0 +1,16 @@
+import React, { createContext } from "react";
+
+const initialState = {
+  user: null,
+};
+
+export const UserContext = createContext(initialState);
+
+export const UserProvider = ({ children }) => {
+  const [state, setState] = React.useState(initialState);
+  return (
+    <UserContext.Provider value={[state, setState]}>
+      {children}
+    </UserContext.Provider>
+  );
+};
