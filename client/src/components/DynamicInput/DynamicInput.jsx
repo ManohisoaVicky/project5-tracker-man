c51d91074454e8514a86611c3b449af1474c5dc8
Manohisoa <manohisoarajaonarivony@gmail.com>
2023-04-05 14:33:36 +0300

    added dynamic input for name and artist

--- client/src/components/DynamicInput/DynamicInput.jsx
+++ client/src/components/DynamicInput/DynamicInput.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import React from "react";
 
-import "./DynamicInput.css"
+function DynamicInput({ field, handleAddFields, handleInputChange, handleRemoveFields, fieldName, placeholder, label, btnTxt }) {
 
-function DynamicInput() {
   return (
-    <div>DynamicInput</div>
-  )
+    <>
+      <div>
+        <label htmlFor={fieldName}>{label}</label>
+        {field.map((element, index) => (
+          <div key={index}>
+            <input
+              type="text"
+              placeholder={placeholder}
+              value={element}
+              onChange={(event) => handleInputChange(fieldName, index, event)}
+            />
+            {index > 0 && (
+              <button
+                type="button"
+                onClick={() => handleRemoveFields(fieldName, index)}
+              >
+                Remove
+              </button>
+            )}
+          </div>
+        ))}
+        <button type="button" onClick={() => handleAddFields(fieldName)}>
+          {btnTxt}
+        </button>
+      </div>
+    </>
+  );
 }
 
-export default DynamicInput
+export default DynamicInput;
--- client/src/pages/TrackPage/TrackPage.jsx
+++ client/src/pages/TrackPage/TrackPage.jsx
@@ -1,11 +1,78 @@
-import React from 'react'
+import React, { useState } from 'react'
 
+import DynamicInput from "../../components/DynamicInput/DynamicInput.jsx";
 import "./TrackPage.css"
 
 function TrackPage() {
+
+    const handleAddFields = (fieldName) => {
+      const values = [...manga[fieldName]];
+      values.push("");
+      setManga({
+        ...manga,
+        [fieldName]: values,
+      });
+    };
+
+    const handleRemoveFields = (fieldName, index) => {
+      const values = [...manga[fieldName]];
+      values.splice(index, 1);
+      setManga({
+        ...manga,
+        [fieldName]: values,
+      });
+    };
+
+    const handleInputChange = (fieldName, index, event) => {
+      const values = [...manga[fieldName]];
+      values[index] = event.target.value;
+      setManga({
+        ...manga,
+        [fieldName]: values,
+      });
+    };
+
+  const [manga, setManga] = useState({
+    name: [""], 
+    artist: [""], 
+    summary: null, 
+    chapRead: null,
+    comicStatus: null, 
+    readingStatus: null, 
+    rating: null, 
+    review: null,
+    tags: []
+  })
+
   return (
-    <div id='trackPage'>TrackPage</div>
-  )
+    <div id="trackPage">
+      <h2>Track a Comic</h2>
+      <form>
+        <DynamicInput
+          setManga={setManga}
+          handleAddFields={handleAddFields}
+          handleRemoveFields={handleRemoveFields}
+          handleInputChange={handleInputChange}
+          field={manga.name}
+          fieldName="name"
+          placeholder="Enter name"
+          label="Name"
+          btnTxt="ADD NAME"
+        />
+        <DynamicInput
+          setManga={setManga}
+          handleAddFields={handleAddFields}
+          handleRemoveFields={handleRemoveFields}
+          handleInputChange={handleInputChange}
+          field={manga.artist}
+          fieldName="artist"
+          placeholder="Enter artist"
+          label="Artist"
+          btnTxt="ADD ARTIST"
+        />
+      </form>
+    </div>
+  );
 }
 
 export default TrackPage
