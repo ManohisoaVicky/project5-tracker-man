1a973bc0db9734ab4d484cc4d3bff46230c72aef
Manohisoa <manohisoarajaonarivony@gmail.com>
2023-05-08 13:48:42 +0300

    updated addtag component to show selected tags

--- client/src/components/AddTag/AddTag.jsx
+++ client/src/components/AddTag/AddTag.jsx
@@ -1,41 +1,69 @@
-import React from 'react'
-import { MANGA_TAGS } from "../../utils/constants"
+import React, { useState } from "react";
+import { MANGA_TAGS } from "../../utils/constants";
 
-import "./AddTag.css"
+import "./AddTag.css";
 
 function AddTag({ manga, setManga }) {
+  const [selectedTags, setSelectedTags] = useState(manga.tags);
 
   function handleTagsChange(e) {
-    const value = e.target.value
-    if (manga.tags.includes(value)) {
-      const index = manga.tags.indexOf(value)
-     setManga((prevState) => ({
-       ...prevState,
-       tags: [...prevState.tags.filter((_, i) => i !== index)],
-     }));
+    const value = e.target.value;
+    let updatedTags = [...selectedTags];
+    if (updatedTags.includes(value)) {
+      updatedTags = updatedTags.filter((tag) => tag !== value);
+    } else {
+      updatedTags.push(value);
     }
-    else {
-      setManga((prevState) => ({
-        ...prevState,
-        tags: [...manga.tags, value],
-      }));
-    }
+    setSelectedTags(updatedTags);
+    setManga((prevState) => ({
+      ...prevState,
+      tags: updatedTags,
+    }));
   }
+
+  function removeTag(index) {
+    const updatedTags = [...selectedTags.filter((_, i) => i !== index)];
+    setSelectedTags(updatedTags);
+    setManga((prevState) => ({
+      ...prevState,
+      tags: updatedTags,
+    }));
+  }
+
+  const availableTags = MANGA_TAGS.filter((tag) => !selectedTags.includes(tag));
 
   return (
     <div id="add_tag_cont">
-      <select multiple value={manga.tags} onChange={handleTagsChange}>
-        <option disabled value="">Add tags</option>
-        {MANGA_TAGS.map((manga) => {
-          return (
-            <option value={manga} key={manga}>
-              {manga}
-            </option>
-          );
-        })}
+      <div id="selected_tag_cont">
+        <ul>
+          {selectedTags.map((tag, index) => (
+            <li key={index}>
+              {tag}
+              <span className="close" onClick={() => removeTag(index)}>
+                &times;
+              </span>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <select
+        multiple
+        value={selectedTags}
+        onChange={handleTagsChange}
+        id="select_input"
+      >
+        <option disabled>──────────</option>
+        <option disabled value="" className="tag_option">
+          Add tags
+        </option>
+        {availableTags.map((tag) => (
+          <option key={tag} value={tag} className="tag_option">
+            {tag}
+          </option>
+        ))}
       </select>
     </div>
   );
 }
 
-export default AddTag
+export default AddTag;
