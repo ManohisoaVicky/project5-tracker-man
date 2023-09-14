8857d2003efffecef13a0197923ffd58e555a30c
Manohisoa <manohisoarajaonarivony@gmail.com>
2023-09-14 15:17:24 +0300

    functionality to get all the reviews for a manga working

--- server/controllers/reviewControllers.js
+++ server/controllers/reviewControllers.js
@@ -28,4 +28,84 @@ async function createReview(req, res, next) {
   }
 }
 
-export { createReview };
+async function getAllReviews(req, res, next) {
+  try {
+    const { page, limit } = req.query;
+    const mangaId = req.params.id;
+
+    const pipeline = [
+      { $match: { _id: mongoose.Types.ObjectId(mangaId) } },
+      {
+        $lookup: {
+          from: "reviews",
+          localField: "review",
+          foreignField: "_id",
+          as: "reviewsData",
+        },
+      },
+      { $unwind: "$reviewsData" },
+      { $skip: (page - 1) * limit },
+      { $limit: parseInt(limit) },
+      { $replaceRoot: { newRoot: "$reviewsData" } },
+    ];
+
+    const [mangaReviews, totalCount] = await Promise.all([
+      Manga.aggregate(pipeline),
+      Manga.findById(mangaId).select("review"),
+    ]);
+
+    if (!mangaReviews) {
+      return res
+        .status(400)
+        .json({ error: true, message: "Manga reviews not found" });
+    }
+
+    const totalPages = Math.ceil(totalCount.review.length / parseInt(limit));
+    res.json({ reviews: mangaReviews, totalPages });
+  } catch (error) {
+    next(error);
+  }
+}
+
+async function getUserMangas(req, res, next) {
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+    const decodedToken = jwt.verify(token, SECRET);
+    const userId = decodedToken.userId;
+    const { page, limit } = req.query;
+
+    const pipeline = [
+      { $match: { _id: mongoose.Types.ObjectId(userId) } },
+      {
+        $lookup: {
+          from: "mangas",
+          localField: "mangas",
+          foreignField: "_id",
+          as: "mangasData",
+        },
+      },
+      { $unwind: "$mangasData" },
+      { $skip: (page - 1) * limit },
+      { $limit: parseInt(limit) },
+      { $replaceRoot: { newRoot: "$mangasData" } },
+    ];
+
+    const [userMangas, totalCount] = await Promise.all([
+      User.aggregate(pipeline),
+      User.findById(userId).select("mangas"),
+    ]);
+
+    if (!userMangas) {
+      return res
+        .status(400)
+        .json({ error: true, message: "User mangas not found." });
+    }
+
+    const totalPages = Math.ceil(totalCount.mangas.length / parseInt(limit));
+    res.json({ mangas: userMangas, totalPages });
+  } catch (error) {
+    next(error);
+  }
+}
+
+export { createReview, getAllReviews };
--- server/routes/reviewsRoutes.js
+++ server/routes/reviewsRoutes.js
@@ -1,11 +1,17 @@
 import express from "express";
-import { createReview } from "../controllers/reviewControllers.js";
+import {
+  createReview,
+  getAllReviews,
+} from "../controllers/reviewControllers.js";
 import { checkAuth } from "../middlewares/checkAuth.js";
 import { checkPermission } from "../middlewares/checkPermission.js";
 
 const router = express.Router();
 
-router.route("/:id").post(checkAuth, createReview);
+router
+  .route("/:id")
+  .post(checkAuth, createReview)
+  .get(checkAuth, getAllReviews);
 
 // router
 //   .route("/:id")
