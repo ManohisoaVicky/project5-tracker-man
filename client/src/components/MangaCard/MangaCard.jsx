d0071f84bd2886634ba6948e9d1f2db61632efca
Manohisoa <manohisoarajaonarivony@gmail.com>
2023-09-27 14:21:36 +0300

    added edit and updated delete styling on mangacard

--- client/src/components/MangaCard/MangaCard.jsx
+++ client/src/components/MangaCard/MangaCard.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 import RatingDisplay from "../RatingDisplay/RatingDisplay.jsx"
 import { capitalizeWords } from "../../utils/utils.js"
-import { FaTrash } from "react-icons/fa";
+import { FaTrash, FaEdit } from "react-icons/fa";
 
 import "./MangaCard.css"
 
@@ -13,26 +13,31 @@ function MangaCard({ manga }) {
     let manga_artist = capitalizeWords(manga.artist[0])
     
   return (
-    <Link to={`/manga/detail/${manga._id}`}>
-      <div className="manga_cont">
-        <div className="manga_info_cont">
-          <div className="manga_main_info">
-            <p>{manga_name}</p>
-            <p>{manga_artist || "N/A"}</p>
+    <div className='manga-card-cont'>
+      <Link to={`/manga/detail/${manga._id}`}>
+        <div className="manga_cont">
+          <div className="manga_info_cont">
+            <div className="manga_main_info">
+              <p>{manga_name}</p>
+              <p>{manga_artist || "N/A"}</p>
+            </div>
+            <div className="manga_info_wrapper">
+              <p className="manga_card_type">
+                Type: <span>{manga.type || "N/A"}</span>
+              </p>
+              <p className="reading_status">
+                Reading Status: <span>{manga.readingStatus || "N/A"}</span>
+              </p>
+            </div>
           </div>
-          <div className="manga_info_wrapper">
-            <p className="manga_card_type">
-              Type: <span>{manga.type || "N/A"}</span>
-            </p>
-            <p className="reading_status">
-              Reading Status: <span>{manga.readingStatus || "N/A"}</span>
-            </p>
-          </div>
+          <RatingDisplay rating={manga.rating} />
         </div>
-        <RatingDisplay rating={manga.rating} />
-        <FaTrash size={20} className='manga-dlt-btn'/>
+      </Link>
+      <div className='manga-card-btn-cont'>
+        <FaEdit size={20} className='manga-edit-btn'/>
+        <FaTrash size={20} className="manga-dlt-btn" />
       </div>
-    </Link>
+    </div>
   );
 }
 
