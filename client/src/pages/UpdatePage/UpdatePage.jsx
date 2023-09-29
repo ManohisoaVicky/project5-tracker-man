2ab622c09130bda910a36ba36c5f492f4d2ab474
Manohisoa <manohisoarajaonarivony@gmail.com>
2023-09-29 13:51:39 +0300

    fetching manga in manga update page

--- client/src/pages/UpdatePage/UpdatePage.jsx
+++ client/src/pages/UpdatePage/UpdatePage.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
 
+import { getSingleManga } from '../../utils/mangaServices'
+import DynamicInput from '../../components/Inputs/DynamicInput/DynamicInput'
 import "./UpdatePage.css"
 
 function UpdatePage() {
+  const [manga, setManga] = useState()
+
+  let mangaID = useParams().mangaID
+
+  useEffect(() => {
+    async function fetchManga() {
+      try {
+        const response = await getSingleManga(mangaID);
+        setManga(response);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    fetchManga();
+  }, [mangaID]);
   return (
-    <div>UpdatePage</div>
+    <div id='update-page-cont'>
+      {manga ? (
+        <p>manga found</p>
+      ): <p>Loading</p>}
+    </div>
   )
 }
 
