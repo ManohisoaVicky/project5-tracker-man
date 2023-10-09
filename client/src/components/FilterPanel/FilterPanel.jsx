66f2c5b3d066d11ccecf9f15c0ffd69f0c1a41dc
Manohisoa <manohisoarajaonarivony@gmail.com>
2023-10-09 14:36:37 +0300

    added the filter and sort icons

--- client/src/components/FilterPanel/FilterPanel.jsx
+++ client/src/components/FilterPanel/FilterPanel.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import { FaFilter } from "react-icons/fa";
 
 import "./FilterPanel.css"
 
 function FilterPanel() {
   return (
-    <div>FilterPanel</div>
+    <div id='filter-container'>
+      <span>Filter</span><FaFilter />
+    </div>
   )
 }
 
-export default FilterPanel
+export default FilterPanel 
--- client/src/components/SortMenu/SortMenu.jsx
+++ client/src/components/SortMenu/SortMenu.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import { FaSort } from "react-icons/fa";
+
 
 import "./SortMenu.css"
 
 function SortMenu() {
   return (
-    <div>SortMenu</div>
-  )
+    <div id='sort-container'>
+      <span>Sort</span><FaSort size={22}/> 
+    </div>
+  );
 }
 
 export default SortMenu
