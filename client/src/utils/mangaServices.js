06cd9ed79b7123183a0415a7a6cb136c56e6479c
Manohisoa <manohisoarajaonarivony@gmail.com>
2023-10-10 14:35:28 +0300

    filter working on frontend

--- client/src/utils/mangaServices.js
+++ client/src/utils/mangaServices.js
@@ -17,20 +17,46 @@ async function trackManga(manga) {
     console.log(error);
   }
 }
-async function fetchMangas(page, search = "", filter = "", sort = "") {
+async function fetchMangas(page, search = "", filter = {}, sort = "") {
   const limit = 10;
 
   try {
     const token = getToken();
-    const apiUrl = `${MANGA_URL}?page=${page}&limit=${limit}&search=${search}&filter=${filter}&sort=${sort}`;
+
+    // Create an array to store the filter criteria
+    const filterCriteria = [];
+
+    // Check if any of the filter options (type, comicStatus, or readingStatus) have values
+    if (filter.types.length > 0) {
+      filterCriteria.push(`type:${filter.types.join(",")}`);
+    }
+
+    if (filter.comicStatus.length > 0) {
+      filterCriteria.push(`comicStatus:${filter.comicStatus.join(",")}`);
+    }
+
+    if (filter.readingStatus.length > 0) {
+      filterCriteria.push(`readingStatus:${filter.readingStatus.join(",")}`);
+    }
+
+    // Combine the filter criteria into a single string with a comma (,) separator
+    const filterString = filterCriteria.join(",");
+
+    // Construct the API URL with the filter portion if there are filter criteria
+    const apiUrl = `${MANGA_URL}?page=${page}&limit=${limit}&search=${search}${
+      filterString ? `&filter=${filterString}` : ""
+    }&sort=${sort}`;
+
     const response = await fetch(apiUrl, {
       headers: {
         Authorization: "Bearer " + token,
       },
     });
+
     if (!response.ok) {
       throw new Error("Failed to fetch mangas");
     }
+
     const data = await response.json();
     return data;
   } catch (error) {
