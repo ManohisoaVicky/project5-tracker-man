49f401738297a20dd509583ebd3dd580f2ae97f0
Manohisoa <manohisoarajaonarivony@gmail.com>
2023-10-11 15:44:02 +0300

    added sort options

--- client/src/components/SortMenu/SortMenu.jsx
+++ client/src/components/SortMenu/SortMenu.jsx
@@ -1,13 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FaSort } from "react-icons/fa";
 
 
 import "./SortMenu.css"
 
 function SortMenu() {
+  const [showSort, setShowSort] = useState(false)
+
+    function displaySort() {
+      setShowSort(!showSort);
+    }
   return (
-    <div id='sort-container'>
-      <span>Sort</span><FaSort size={22}/> 
+    <div id="sort-container">
+      <div id="sort-icons-cont" onClick={displaySort}>
+        <span>Sort</span>
+        <FaSort size={22} />
+      </div>
+      <div id={showSort ? "sort-show" : "sort-hidden"}>
+        <div id="sort-options">
+          <p>Name</p>
+          <p>Artist</p>
+          <p>Rating</p>
+        </div>
+        <div id="sort-order">
+          <p>ASC</p>
+          <p>DESC</p>
+        </div>
+      </div>
     </div>
   );
 }
