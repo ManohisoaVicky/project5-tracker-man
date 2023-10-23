ba63e7e0b0d407fa5becdefbdb02bdf8b98b72f4
Manohisoa <manohisoarajaonarivony@gmail.com>
2023-10-23 11:52:08 +0300

    added to, from and total mangas in getusermangas controller

--- server/controllers/mangaControllers.js
+++ server/controllers/mangaControllers.js
@@ -40,49 +40,11 @@ async function getUserMangas(req, res, next) {
     }
 
     if (filter) {
-      const filters = filter.split(",").map((filterItem) => {
-        const [key, ...values] = filterItem.split(":");
-        if (key === "rating") {
-          // Handle numeric comparisons for the "rating" field
-          if (values.length === 1) {
-            return { [`mangasData.${key}`]: parseInt(values[0]) };
-          } else if (values.length === 2 && values[0] === "gte") {
-            return { [`mangasData.${key}`]: { $gte: parseInt(values[1]) } };
-          } else if (values.length === 2 && values[0] === "lte") {
-            return { [`mangasData.${key}`]: { $lte: parseInt(values[1]) } };
-          }
-        } else if (["type", "comicStatus", "readingStatus"].includes(key)) {
-          return { [`mangasData.${key}`]: values.join(":").trim() };
-        }
-        return null;
-      });
-
-      // Filter out null values and push valid filters to the pipeline
-      const validFilters = filters.filter((filter) => filter !== null);
-      if (validFilters.length > 0) {
-        pipeline.push({ $match: { $and: validFilters } });
-      }
+      // ... (Your existing code for filtering)
     }
 
     if (sort) {
-      // Split the sort string into sortField and sortOrder
-      const [sortField, sortOrder] = sort.split(":");
-
-      // Define a sort order value based on sortOrder (default to 1 for ascending)
-      const sortOrderValue = sortOrder === "desc" ? -1 : 1;
-
-      // Define a sort field mapping
-      const sortFieldMapping = {
-        name: "mangasData.name",
-        artist: "mangasData.artist",
-        rating: "mangasData.rating",
-      };
-
-      // Check if the sortField is a valid key
-      if (sortField in sortFieldMapping) {
-        const sortFieldKey = sortFieldMapping[sortField];
-        pipeline.push({ $sort: { [sortFieldKey]: sortOrderValue } });
-      }
+      // ... (Your existing code for sorting)
     }
 
     const skip = (page - 1) * limit;
@@ -102,12 +64,19 @@ async function getUserMangas(req, res, next) {
     // Extract the total count from the aggregation result
     const total = totalMangasCount.length > 0 ? totalMangasCount[0].total : 0;
 
-    // Calculate totalPages based on the total count and limit
-    const totalPages = Math.ceil(total / limitCount);
-
     const userMangas = await User.aggregate(pipeline);
 
-    res.json({ mangas: userMangas, totalPages });
+    const to = skip + 1;
+    const from = Math.min(skip + limitCount, total);
+    const totalMangas = total;
+
+    res.json({
+      mangas: userMangas,
+      totalPages: Math.ceil(total / limitCount),
+      to,
+      from,
+      totalMangas,
+    });
   } catch (error) {
     next(error);
   }
