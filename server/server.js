529e30d5612ffa0daf51fe3d75ad439cafc0d6b7
Manohisoa <manohisoarajaonarivony@gmail.com>
2023-10-27 13:29:27 +0300

    added frontend link to cors

--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:3000",
-      "https://snazzy-snickerdoodle-f7b920.netlify.app/",
+      "https://trackerman.netlify.app/",
       "https://trackerman-backend.onrender.com",
     ],
   })
