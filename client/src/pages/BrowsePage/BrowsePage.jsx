425bae30998f5c04d235ac47a651a551880da4a2
Manohisoa <manohisoarajaonarivony@gmail.com>
2023-11-07 13:11:31 +0300

    browse feature coming soon

--- client/src/pages/BrowsePage/BrowsePage.jsx
+++ client/src/pages/BrowsePage/BrowsePage.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 
+import "./BrowsePage.css"
+
 function BrowsePage() {
   return (
-    <div>BrowsePage</div>
+    <div id='browse-cont'>
+      <p>Browse feature coming soong!</p>
+    </div>
   )
 }
 
